8e6fb8299e4a35fb8ece76ad63da89e5ccd2ab2c
Sharath H K <158291023+Sharathhk122@users.noreply.github.com>
2025-06-04 19:45:09 +0530

    Update axios.js

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://bidpulse-server10.onrender.com//api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://bidpulse-server10.onrender.com',
   withCredentials: true
 });
 
