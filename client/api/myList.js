f7453e6f22168507952b46abb300100424733b2c
Sharath H K <158291023+Sharathhk122@users.noreply.github.com>
2025-06-06 19:48:05 +0530

    Update myList.js

--- client/api/myList.js
+++ client/api/myList.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 export const getMyList = async () => {
-  const response = await axios.get('/api/mylist');
+   const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/mylist`);
   return response.data;
 };
 
