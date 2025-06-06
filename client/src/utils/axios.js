38d560f021c126c7d695351efdff822fab6efc3d
Sharath H K <158291023+Sharathhk122@users.noreply.github.com>
2025-06-06 12:09:27 +0530

    Update axios.js

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const instance = axios.create({
-  baseURL: 'https://bidpulse-server10.onrender.com/api'
+  baseURL: 'https://bidpulse-real-time-pulse-of-competitive-9cfm.onrender.com/api'
 });
 
 instance.interceptors.request.use((config) => {
