04e515779a5b05c4880ac8028b30f3f689976b78
Sharath H K <158291023+Sharathhk122@users.noreply.github.com>
2025-06-06 20:26:02 +0530

    Update AuthContext.jsx

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -11,7 +11,7 @@ export function AuthProvider({ children }) {
 
   const login = async (emailOrUsername, password) => {
     try {
-      const { data } = await axios.post('/auth/login', {
+      const { data } = await axios.post(`${import.meta.env.VITE_API_URL}/auth/login`, {
         emailOrUsername,
         password
       });
