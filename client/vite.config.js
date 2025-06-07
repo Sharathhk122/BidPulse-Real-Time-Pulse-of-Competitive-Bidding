3142a6a63ae0b5e263a7159873b3d9ca1928cb26
Sharath H K <158291023+Sharathhk122@users.noreply.github.com>
2025-06-07 09:26:27 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://bidpulse-real-time-pulse-of-competitive-l69x.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
